ddcc9209c45657c42aaa76c6d215b7edcbcbe442
Wahyudin251 <114757207+Wahyudin251@users.noreply.github.com>
2024-10-23 17:46:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const sidebar = document.querySelector('.sidebar');
+const menuToggle = document.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
